b74c420cfdc982efb64063672d3c91473b0c975e
NicolasBiojo <nikobiober@gmail.com>
2019-03-12 11:09:43 -0500

    Add server

---
+++ Agar.io/src/server/Server.java
@@ -0,0 +1,91 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+
+	private ArrayList<Server_Receive_Thread> serverRecive;
+	private Server_Send_Thread serverSend;
+
+	private ArrayList<String> messeges;
+	private ArrayList<Socket> sockets;
+
+	private static ServerSocket serverSocket;
+	private boolean sendMulticast;
+
+	private int sender;
+
+	public Server(int port, int number_clients) {
+
+		try {
+
+			System.out.println("Server on line");
+			serverSocket = new ServerSocket(port);
+			sendMulticast = false;
+			sender = -1;
+
+			sockets = new ArrayList<>();
+			messeges = new ArrayList<>();
+
+			serverRecive = new ArrayList<>();
+
+			for (int i = 0; i < number_clients; i++) {
+				Socket s = serverSocket.accept();
+				sockets.add(s);
+				System.out.println("Client was connected");
+				serverRecive.add(new Server_Receive_Thread(this, s, i));
+			}
+
+			serverSend = new Server_Send_Thread(this);
+			serverSend.start();
+
+			for (int i = 0; i < serverRecive.size(); i++) {
+				serverRecive.get(i).start();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public ArrayList<Socket> getSockets() {
+		return sockets;
+	}
+
+	public boolean isSendMulticast() {
+		return sendMulticast;
+	}
+
+	public void setSendMulticast(boolean sendMulticast) {
+		this.sendMulticast = sendMulticast;
+	}
+
+	public int getSender() {
+		return sender;
+	}
+
+	public void setSender(int sender) {
+		this.sender = sender;
+	}
+
+	public void newMessege(String mensajeObtenidoCliente) {
+		messeges.add(mensajeObtenidoCliente);
+	}
+
+	public String lastMessage() {
+		if (messeges.size() > 0) {
+			return messeges.get(messeges.size() - 1);
+		}
+		return "";
+	}
+
+	public static void main(String[] args) {
+
+		Server s = new Server(8000, 3);
+
+	}
+}
---
+++ Agar.io/src/server/Server_Receive_Thread.java
@@ -0,0 +1,43 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server_Receive_Thread extends Thread {
+
+	private Server server;
+	private Socket socket;
+	private int number;
+
+	public Server_Receive_Thread(Server server, Socket socket, int number) {
+
+		this.server = server;
+		this.socket = socket;
+		this.number = number;
+	}
+
+	public void run() {
+
+		try {
+
+			DataInputStream in;
+			System.out.println("start recive thread");
+			while (true) {
+
+				in = new DataInputStream(socket.getInputStream());
+				String messege = in.readUTF();
+				System.out.println(messege);
+				server.setSender(number);
+				server.newMessege(messege);
+				server.setSendMulticast(true);
+
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+
+}
