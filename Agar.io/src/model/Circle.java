ea2b608a0a79097aa4855e4d6153732adb65eb62
NicolasBiojo <nikobiober@gmail.com>
2019-03-21 19:19:10 -0500

    Declare the classes circle and player

--- Agar.io/src/model/Circle.java
+++ Agar.io/src/model/Circle.java
@@ -1,5 +1,30 @@
 package model;
 
+import java.awt.Color;
+import java.awt.Point;
+
 public class Circle {
 
+	private Color color;
+	private Point position;
+	private int radius;
+	
+	public Circle(Color color, Point position, int radius) {
+		this.color = color;
+		this.position = position;
+		this.radius = radius;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public Point getPosition() {
+		return position;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+	
 }
--- Agar.io/src/model/Player.java
+++ Agar.io/src/model/Player.java
@@ -1,5 +1,25 @@
 package model;
 
+import java.awt.Color;
+import java.awt.Point;
+
 public class Player extends Circle {
+
+	public final static String ALIVE = "ALIVE";
+	public final static String GAME_OVER = "GAME_OVER";
+	public String state;
+
+	public Player(Color color, Point position, int radius) {
+		super(color, position, radius);
+
+	}
+
+	public void move() {
+
+	}
+
+	public void grow() {
+
+	}
 
 }
