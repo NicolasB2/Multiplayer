bf2cd1adacb69303306f4382dc4608cdea5296a9
NicolasBiojo <nikobiober@gmail.com>
2019-03-21 19:24:17 -0500

    Declare the User class

--- Agar.io/src/model/Player.java
+++ Agar.io/src/model/Player.java
@@ -8,6 +8,8 @@ public class Player extends Circle {
 	public final static String ALIVE = "ALIVE";
 	public final static String GAME_OVER = "GAME_OVER";
 	public String state;
+	
+	private String name;
 
 	public Player(Color color, Point position, int radius) {
 		super(color, position, radius);
--- Agar.io/src/model/User.java
+++ Agar.io/src/model/User.java
@@ -2,5 +2,35 @@ package model;
 
 public class User {
 
+	private String nickname;
+	private String password;
+	private String email;
 	private Player player;
+	
+	public User(String nickname, String password, String email) {
+		
+		this.nickname = nickname;
+		this.password = password;
+		this.email = email;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+	
+	
+	
+	
 }
