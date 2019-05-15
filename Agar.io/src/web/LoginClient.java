fa9f6e9e9813011b3ae2de52c5b26af1fb1d2d10
saradrada <saradrada@hotmail.com>
2019-05-15 10:26:28 -0500

    Add login web service

---
+++ Agar.io/src/web/LoginClient.java
@@ -0,0 +1,45 @@
+package web;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class LoginClient {
+	private Socket socket;
+	private String email, password;
+
+	public LoginClient(String email, String password) {
+		this.email = email;
+		this.password = password;
+
+	}
+
+	public boolean validateLogin() {
+		String answer = "";
+		try {
+			this.socket = new Socket("localhost", 8081);
+
+			DataInputStream in;
+			DataOutputStream out;
+
+			in = new DataInputStream(socket.getInputStream());
+			out = new DataOutputStream(socket.getOutputStream());
+
+			out.writeUTF(this.email);
+			out.writeUTF(this.password);
+
+			answer = in.readUTF();
+
+		} catch (Exception e) {
+		}
+
+		if (answer.equals("yes")) {
+			return true;
+		}
+
+		if (answer.equals("no")) {
+			return false;
+		}
+		return false;
+	}
+}
